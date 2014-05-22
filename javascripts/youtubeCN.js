caff7df23f86ed2f2cd286b34bc8ec4696cc4b65
Zhantong Zhang <zhang@zhantong.org>
2014-05-22 00:11:24 -0600

    Site updated at 2014-05-22 06:11:24 UTC

--- javascripts/youtubeCN.js
+++
@@ -1,9 +0,0 @@
-var icon = "http://s.ytimg.com/yts/img/favicon-vfldLzJxy.ico";
-var locationWall = (! $('<img src="'+ icon +'">').load()); // false if out of Mainland; true otherwise
-var plid="td"; // remove tudou if outside of Mainland
-if(locationWall)
-{
-  plid="yt" // remove youtube if inside of Mainland
-}
-var player=document.getElementById(plid);
-player.parentNode.removeChild(player);
